15bfc838ea4bb33acdcf9cca6610d1ca52534232
JorgeAller <jorgeallertaboada@hotmail.com>
2023-05-05 13:59:58 +0200

    Initialize new dependencies, and inital tries

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3306/sections/3");
+      const data = await response.json();
+      console.log(data.data.section);
+      setSections(data.data.section);
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {sections.map((section) => (
+        <div key={section.id}>
+          <h1>{section.id}</h1>
+          <h2>{section.title}</h2>
+          <h3>{section.place}</h3>
+          <h3>{section.date}</h3>
+          <h3>{section.hour}</h3>
+          <p>{section.duration} min</p>
+          <p>{section.bio}</p>
+          <p>{section.cur_text}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
